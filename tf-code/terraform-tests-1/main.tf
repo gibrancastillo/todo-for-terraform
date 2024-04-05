99deab445507f6b068586f81c42038eee02a913a
Gibran Castillo <gibran-castillo@hotmail.com>
2024-04-05 09:42:58 -0600

    Add your changes

--- tf-code/terraform-tests-1/main.tf
+++ tf-code/terraform-tests-1/main.tf
@@ -8,24 +8,41 @@ terraform {
 }
 
 provider "todo" {
-  host = "127.0.0.1"
+  # host = "127.0.0.1"
+  host = "todo-api.techlabs.sh"
   port = "8080"
   apipath = "/"
   schema = "http"
 }
 
 data "todo_todo" "foreign" {
-  id = 1
+  id = 8
 }
 
 resource "todo_todo" "test1" {
-  count = 5
+  count = 4
   description = "${count.index}-1 ${var.purpose} todo"
   completed = false
 }
 
 resource "todo_todo" "test2" {
-  count = 5
+  count = 4
   description = "${count.index}-2 ${var.purpose} todo (linked to ${data.todo_todo.foreign.description})"
   completed = false
+}
+
+resource "todo_todo" "primary" {
+  description = "save the world"
+  completed = false
+}
+
+module "series-data" {
+  source = "../__modules/todo-test-data"
+  number = 5
+  purpose = "testing"
+  team_name = "oreilly"
+  descriptions = ["my first completed todo", "my second completed todo",
+                  "my third completed todo", "my fourth completed todo",
+                  "my fifth completed todo"
+                 ]
 }
--- tf-code/terraform-tests-1/outputs.tf
+++ tf-code/terraform-tests-1/outputs.tf
@@ -5,3 +5,10 @@ output "todo_1_ids" {
 output "todo_2_ids" {
   value = todo_todo.test2.*.id
 }
+
+output "first_series_ids" {
+  value = module.series-data.first_series_ids
+}
+output "second_series_ids" {
+  value = module.series-data.second_series_ids
+}
